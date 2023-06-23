db674da7286c93a694b50792700ae64dfa9b6945
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-06-23 03:56:14 -0700

    bubble sort fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,14 +9,14 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t j, i;
-	int temp, swap;
+	int temp;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swap = 0;/*track the inner loop for a swap*/
+		/*track the inner loop for a swap*/
 
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -26,21 +26,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swap = 1;
 				print_array(array, size);
 			}
 		}
 
-		/* printing array after each swap */
-		for (j = 0; j < size; j++)
-		{
-			printf("%d ", array[j]);
-		}
-
-		printf("\n");
-
-		/* condition on a no swap occuring */
-		if (swap == 0)
-			break;
 	}
 }
