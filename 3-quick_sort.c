bbb949d800d40ee9f2b731edd8df3a3c5604c7fa
Oziegbe Daniel <oziegbeosy@gmail.com>
2023-06-25 13:58:13 -0700

    3-quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,7 +6,6 @@
  * @array: Pointer to the array
  * @size: Size of the array
  */
-
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
@@ -25,49 +24,57 @@ void quicksort_recursive(int *array, int low, int high)
 {
 	if (low < high)
 	{
-		int pivot = lomuto_partition(array, low, high);
+		int pivot = lomuto_partition(array, low, high, high - low + 1);
 
 		quicksort_recursive(array, low, pivot - 1);
 		quicksort_recursive(array, pivot + 1, high);
 	}
 }
 
+
 /**
- * lomuto_partition - Lomuto partition scheme for Quick sort
- * @array: Pointer to the array
- * @low: Lowest index of the partition
- * @high: Highest index of the partition
+ * lomuto_partition - Lomuto partition scheme
+ * @array: The array to be sorted
+ * @low: Starting index of the partition to be sorted
+ * @high: Ending index of the partition to be sorted
+ * @size: Size of the array
  *
  * Return: Index of the pivot element
  */
-int lomuto_partition(int *array, int low, int high)
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
-	int j, temp;
+	int i = low - 1, j;
 
-	for (j = low; j < high; j++)
+	for (j = low; j <= high - 1; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
 			i++;
-			if (i != j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, high + 1);
-			}
+			swap(&array[i], &array[j]);
+			print_array(array, size);
 		}
 	}
 
-	if (i + 1 != high)
+	if (array[high] < array[i + 1])
 	{
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
-		print_array(array, high + 1);
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
 	}
 
 	return (i + 1);
+}
+
+
+/**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
